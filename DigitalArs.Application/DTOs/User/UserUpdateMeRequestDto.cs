a6c5feb456ece9fc49b498eba8b62fd0e8c961b8
Maria-Wagner <wagnermaria1012@gmail.com>
2026-09-01 20:45:33 -0300

    feat: HU-13 Completada - GET y PUT

---
+++ DigitalArs.Application/DTOs/User/UserUpdateMeRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DigitalArs.Application.DTOs.User;
+
+public class UserUpdateMeRequestDto
+{
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; } = "";
+
+    public string? CurrentPassword { get; set; }
+    public string? NewPassword { get; set; }
+}
