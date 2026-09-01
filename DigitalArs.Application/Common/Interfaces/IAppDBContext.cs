39455b125cbe532726bfd76e7294f189570b466a
Maria-Wagner <wagnermaria1012@gmail.com>
2026-09-01 17:52:38 -0300

    HU-12 CRUD admin users completo

---
+++ DigitalArs.Application/Common/Interfaces/IAppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using DigitalArs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalArs.Application.Common.DTOs.Interfaces;
+
+public interface IAppDbContext
+{
+    DbSet<User> Users { get; }
+    DbSet<Role> Roles { get; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
--- DigitalArs.Application/DTOs/User/UserResponseDto.cs
+++ DigitalArs.Application/DTOs/User/UserResponseDto.cs
@@ -6,4 +6,6 @@ public class UserResponseDto
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string RoleName { get; set; } = string.Empty;
+    public int RoleId { get; set; }
+    public bool IsActive { get; set; }
 }
